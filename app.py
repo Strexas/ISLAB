71acae4a0c102c17d0a846936f7f25a166d15f07
KouniakiosKatakthths <theofaniskouniakis@gmail.com>
2025-12-01 11:14:15 +0200

    New tables migrations

--- app.py
+++ app.py
@@ -2,16 +2,24 @@ from flask import Flask
 from user_management.routes import user_management_bp as user_management_blueprint
 from reservation_subsystem.routes import reservation_subsystem
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 db = SQLAlchemy()
 
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://db_user:2@localhost:5432/car_rental"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+migrate = Migrate(app, db)
+
+db.init_app(app)
+
+app.register_blueprint(user_management_blueprint)
+app.register_blueprint(reservation_subsystem)
+
 if __name__ == '__main__':
-    app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://db_user:2@localhost:5432/car_rental"
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-    db.init_app(app)
-
-    app.register_blueprint(user_management_blueprint)
-    app.register_blueprint(reservation_subsystem)
     app.run(debug=True)
+
+
+from models.Reservation import Reservation as Reservation
+from models.InsurancePolicy import InsurancePolicy as InsurancePolicy
--- reservation_subsystem/routes.py
+++ reservation_subsystem/routes.py
@@ -1,14 +1,14 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session
 
 reservation_subsystem = Blueprint("reservations", __name__)
 
 @reservation_subsystem.route("/", methods=["GET"])
 def index():
-    return render_template("index.html")
+    return render_template("index.html", logged_in=('user_id' in session))
 
 @reservation_subsystem.route("/fleetlist", methods=["GET"])
 def fleet_list():
-    return render_template("fleetlist.html")
+    return render_template("fleetlist.html", logged_in=('user_id' in session))
 
 @reservation_subsystem.route("/car_details/<int:vehicle_id>", methods=["GET"])
 def car_details(vehicle_id):
