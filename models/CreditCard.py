41fb371f5096b6dd7186a400fb6c7c34fd46dcef
Vatan Çulhaoğlu <vatanculhaoglu@gmail.com>
2025-12-07 19:38:10 +0200

    Add CreditCard model to manage credit card data

---
+++ models/CreditCard.py
@@ -0,0 +1,16 @@
+from context import db
+
+
+class CreditCard(db.Model):
+    __tablename__ = "creditcard"
+
+    cardid = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    userid = db.Column(db.Integer, db.ForeignKey("user.userid"), nullable=False)
+
+    cardholdername = db.Column(db.String(100), nullable=False)
+    cardnumber = db.Column(db.String(20), nullable=False)
+    expiredate = db.Column(db.Date, nullable=False)
+    billingaddress = db.Column(db.String(255), nullable=False)
+    added = db.Column(db.Date, nullable=False)
+
+    user = db.relationship("User", backref="creditcards")
