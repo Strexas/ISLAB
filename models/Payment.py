0a498d7c0483905ebe3e2d5c8d0619e77c5a4925
Vatan Çulhaoğlu <vatanculhaoglu@gmail.com>
2025-12-08 00:06:33 +0200

    Refactor Payment model fields and foreign keys

--- models/Payment.py
+++ models/Payment.py
@@ -14,14 +14,24 @@ class PaymentStatus(Enum):
 class Payment(db.Model):
     __tablename__ = "payment"
 
-
     paymentid = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    userid = db.Column(db.Integer, db.ForeignKey("user.userid"), nullable=False)
-    reservationid = db.Column(db.Integer, db.ForeignKey("reservation.reservationid"), nullable=True)
+
+    userid = db.Column(db.Integer,
+                       db.ForeignKey("users.id"),
+                       nullable=False)
+
+    reservationid = db.Column(db.String(36),
+                              db.ForeignKey("reservations.reservation_id"),
+                              nullable=True)
 
     date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     amount = db.Column(db.Float, nullable=False)
 
-    status = db.Column(PgEnum(PaymentStatus), nullable=False, default=PaymentStatus.PENDING)
+    status = db.Column(
+        PgEnum(PaymentStatus, name="paymentstatus"),
+        nullable=False,
+        default=PaymentStatus.PENDING
+    )
+
     paymentmethod = db.Column(db.String(20), nullable=False)  # "Card" / "Balance"
     description = db.Column(db.String(255), nullable=True)
